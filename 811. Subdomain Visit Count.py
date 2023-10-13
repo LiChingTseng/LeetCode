8fb541ebeab1881678e4e80b6972dc02e00568df
LiChingTseng <147107274+LiChingTseng@users.noreply.github.com>
2023-10-13 15:31:29 -0400

    Update 811. Subdomain Visit Count.py

--- 811. Subdomain Visit Count.py
+++ 811. Subdomain Visit Count.py
@@ -13,11 +13,15 @@ class Solution(object):
         ans = collections.Counter()
         
         for domain in cpdomains:
-            count, domain = domain.split()
-            ans[domain] += int(count)
+            count, domain = domain.split() # space between count and domain
+            ans[domain] += int(count) # domain is key, storing count of domain
 
+            # check subdomains in domain
             for i in range(len(domain)):
                 if domain[i] == '.':
-                    ans[domain[i+1: ]] += int(count)
+                    ans[domain[i+1: ]] += int(count) # create new key 
         
-        return ["%d %s" % (ans[k], k) for k in ans]
+        return ["%d %s" % (ans[k], k) for k in ans] 
+        # %d and %s are placeholders for an integer and a number
+        #   ans[k] replace %d, k repalce %s
+        #   % operator is used to replace the placeholders in the string with the values from the tuple. 
