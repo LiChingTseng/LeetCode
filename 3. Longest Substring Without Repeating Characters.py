b4bc4921cf9d42714de059f63f359f2fe8c66da9
LiChingTseng <147107274+LiChingTseng@users.noreply.github.com>
2023-10-13 17:41:50 -0400

    Update 3. Longest Substring Without Repeating Characters.py

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -6,15 +6,17 @@ class Solution(object):
         """
 
         # total number of letters, digits, symbols and spaces
-        # charIndex record the indices of characters
+        # charIndex record the indices of characters pointed by right pointer
         charIndex = [-1] * 128 
         left = 0
         maxlength = 0
 
         for right in range(len(s)):
-            # check if the character has occurred
+            # check if the character pointed by left is same as that by right pointer
             if charIndex[ ord(s[right])] >= left:
-                left = charIndex[ ord(s[right])] + 1
+                left = charIndex[ ord(s[right])] + 1 
+                # move left pointer to the next index of the charatcter that 
+                #   right pointer had pointed
 
             charIndex[ ord(s[right])] = right
             maxlength = max(maxlength, right-left+1)
